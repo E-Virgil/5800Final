9fc1d4a092e305aa7b874737a89c9c28dde4bb62
E-Virgil <virgil.e@northeastern.edu>
2023-12-14 01:02:45 -0500

    Added some result pages just to be safe

--- Controller/test.py
+++ Controller/test.py
@@ -1,35 +1,30 @@
 # this file was written, in its entirety at this point, by Sayantan Datta, I (Ethan Virgil) uploaded it as he was having trouble pushing to this repo
 from flask import Flask, render_template, request
-
+from Model.CheapestFlightSearcher import CheapestFlightSearcher
+from Model.adjacencymatrix import AdjacencyMatrix
+matrix = AdjacencyMatrix("Model/Flight Data - Sheet1.csv")
+airport_code_to_index_dict = {flight_label: index for index, flight_label in enumerate(matrix.get_vertices())}
+usd_searcher = CheapestFlightSearcher(matrix.get_USD_matrix(), airport_code_to_index_dict)
+ghg_searcher = CheapestFlightSearcher(matrix.get_GHG_matrix(), airport_code_to_index_dict)
 app = Flask(__name__)
+
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  # Check if the form was submitted
-  if request.method == 'POST':
-    # Get the selected source and destination from the form data
-    source = request.form.get('source')
-    destination = request.form.get('destination')
+  return render_template('UI.html')
 
-    # Store the values in a Python variable
-    flight_data = {
-      'source': source,
-      'destination': destination,
-    }
+@app.route('/result', methods=['GET'])
+def result():
+    destination = request.args.get('destination')
+    origin = request.args.get('origin')
+    path = usd_searcher.search(origin, destination)
 
-    # Print the selected source and destination for debugging purposes
-    print(f"Selected source: {flight_data['source']}")
-    print(f"Selected destination: {flight_data['destination']}")
 
-    # Further processing with flight_data (e.g., search for flights)
-    # ... (your flight search logic here)
+    print(f"Origin: {origin}")
+    print(f"Destination: {destination}")
 
-    # Return the template with a message if desired
-    return render_template('UI.html', message="Flight data received!")
-  else:
-    # Render the form if it wasn't submitted
-    return render_template('UI.html')
+    return render_template('result.html', origin=origin, destination=destination)
 
 if __name__ == "__main__":
   app.run(debug=True)
